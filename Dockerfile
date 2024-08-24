96fa0b15c675fdeb8f4247507be47850bd82adba
James Sheputis <git@jsheputis.com>
2024-08-24 14:09:11 -0500

    Test

--- Dockerfile
+++ Dockerfile
@@ -10,12 +10,16 @@ ENV LANG C.UTF-8
 
 RUN mkdir -p /var/www/html
 
-RUN mkdir /merge-m3u
+RUN mkdir -p /merge-m3u/data && chmod 755 /merge-m3u
 
 COPY merge-m3u.py /merge-m3u/
 RUN chmod a+x /merge-m3u/merge-m3u.py
 
+WORKDIR /merge-m3u
+RUN wget -O xteve https://github.com/xteve-project/xTeVe-Downloads/blob/master/xteve_linux_amd64.zip?raw=true && chmod a+x xteve
+
 COPY run.sh /run.sh
+
 RUN chmod a+x /run.sh
 
 COPY xteve-filtered.m3u /var/www/html/
--- merge-m3u.py
+++ merge-m3u.py
@@ -4,15 +4,20 @@
 import requests
 import os
 import re
+from pathlib import Path
 
 USERNAME = os.getenv('M3U_USERNAME')
 PASSWORD = os.getenv('M3U_PASSWORD')
 BASE_PATH_OVERRIDE = os.getenv('M3U_BASE_PATH')
+WORKING_SPACE_DIR_BASE_PATH_OVERRIDE = os.getenv('M3U_WORKING_SPACE_DIR_BASE_PATH')
 
 # BASE_URL = 'https://tvnow.best/api/list/%s/%s/m3u8' % (USERNAME, PASSWORD)
 BASE_URL = 'http://xkdhhfy.mmastertv.xyz/get.php?username=%s&password=%s&output=ts&type=m3u_plus' % (USERNAME, PASSWORD)
 
 BASE_PATH = BASE_PATH_OVERRIDE if None != BASE_PATH_OVERRIDE else '/var/www/html'
+WORKING_SPACE_DIR_BASE_PATH_OVERRIDE = WORKING_SPACE_DIR_BASE_PATH_OVERRIDE if None != WORKING_SPACE_DIR_BASE_PATH_OVERRIDE else '/m3u-merge/data'
+
+EPG_URL = 'https://epgshare01.online/epgshare01/epg_ripper_UK1.xml.gz'
 
 HEADERS = {
     'User-Agent': 'M3U',
@@ -20,8 +25,15 @@ HEADERS = {
     'Connection': 'Keep-Alive'
 }
 
-print(BASE_URL)
-print(BASE_PATH)
+print("Configuring workspace...")
+print(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE)
+Path(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE).mkdir(parents=True, exist_ok=True)
+
+
+
+# if not os.path.exists(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE):
+#     print("Creating working dir... %s", WORKING_SPACE_DIR_BASE_PATH_OVERRIDE)
+#     os.makedirs(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE)
 
 # try:
 #     os.remove(BASE_PATH + '/all.m3u.wip')
@@ -34,8 +46,9 @@ print(BASE_PATH)
 #     exit(0)
 # # Configure Live TV
 
+print("Processing M3U data...")
 REGEX_TEST = re.compile('#EXTINF.*?tvg-id=".*\.us"')
-with open(BASE_PATH + '/all.m3u.wip', 'w') as outfile:
+with open(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE + '/all-simplified.m3u.wip', 'w') as outfile:
     contents = requests.get(BASE_URL, headers=HEADERS).text.splitlines()
     print("File read. %d records." % len(contents))
     
@@ -55,9 +68,16 @@ with open(BASE_PATH + '/all.m3u.wip', 'w') as outfile:
         i = i+1
 
 
-os.rename(BASE_PATH + '/all.m3u.wip', BASE_PATH + '/all2.m3u')
+os.rename(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE + '/all-simplified.m3u.wip', BASE_PATH + '/all-simplified.m3u')
+print("M3U File creation complete.")
 
-print("File creation complete.")
+print("Processing XMLTV data...")
+with open(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE + '/xmltv.xml.gz.wip') as outfile:
+    contents = requests.get(EPG_URL, headers=HEADERS).text
+    outfile.write(contents)
+
+os.rename(WORKING_SPACE_DIR_BASE_PATH_OVERRIDE + '/xmltv.xml.gz.wip', BASE_PATH + '/xmltv.xml.gz')
+print("XMLTV File creation complete.")
 # # Configure Movies
 # try:
 #     os.remove(BASE_PATH + '/movies.m3u.wip')
